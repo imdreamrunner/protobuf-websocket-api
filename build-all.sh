85a218f904bffeb42ccc906493dc2bb435d67e4f
Ivor Zhou <ivorz@sg.ibm.com>
2017-09-28 10:51:00 +0800

    Fix build-all.sh script.

--- build-all.sh
+++ build-all.sh
@@ -1,15 +1,18 @@
 set -e
 
 cd cli
+npm install
 npm run build
 cd ..
 
 cd server
+npm install
 npm run build
 cd ..
 
 cd examples/simple-api-server
-npm run reinstall-pwa
+rm -rf node_modules
+npm install
 npm run build-proto
 npm run build-api
 npm run build
