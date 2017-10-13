ae7f48774718179c35673193db4c321a0464d723
Ivor Zhou <ivorz@sg.ibm.com>
2017-10-13 16:28:48 +0800

    Add server support for services.

--- examples/simple-api-server/src/index.ts
+++ examples/simple-api-server/src/index.ts
@@ -1,9 +1,8 @@
 import handleRequest from "protobuf-websocket-api-server";
-import { handlers, endpoints } from "./server";
 import * as ws from "ws";
 
 const port: number = 23333;
 
 const server = new ws.Server({ port: port });
-handleRequest(server, handlers, endpoints);
+handleRequest(server);
 console.log(`WebSocket server started at port ${port}.`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,81 @@
 import * as ws from "ws";
+import * as fs from "async-file";
+import * as protobuf from "protobufjs";
 import * as schema from "../schema";
 
-export interface HandlerMap {
-    [handlerName: string]: {};
-}
-
 export interface Endpoint {
-    moduleName: string;
-    apiName: string;
-    requestSchema: any;
-    responseSchema: any;
+    methodName: string;  // the method in the request package.
+    handlerModuleName: string;  // the JavaScript module name of the actual implementation.
+    handlerMethodName: string;  // the JavaScript method name.
+    requestSchema: any;  // the request Protobuf object.
+    responseSchema: any;  // the response Protobuf object.
 }
 
-export default function handleRequest(server: ws.Server, handlers: HandlerMap, endpoints: Endpoint[]) {
+const PROJECT_CWD = process.cwd();
+
+async function loadProtobuf(filename: string|string[]): Promise<protobuf.Root> {
+    return new Promise<protobuf.Root>((resolve, reject) => {
+        protobuf.load(filename, (err, root) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(root);
+            }
+        });
+    });
+}
+
+function getAllServices(root: protobuf.Root): string[] {
+    console.log("Name", root.fullName);
+    const result: string[] = [];
+    if (root.nestedArray.length > 0) {
+        for (const nested of root.nestedArray) {
+            result.push(...getAllServices(<protobuf.Root>nested));
+        }
+    } else {
+        result.push(root.fullName);
+    }
+    return result;
+}
+
+async function initializeEndpoints() {
+    const projectDefinition = JSON.parse(await fs.readFile(`${PROJECT_CWD}/package.json`));
+    const protoDir = projectDefinition["apiSchema"];
+
+    const serviceFiles = await fs.readdir(`${protoDir}/services`);
+    const schemaFiles = await fs.readdir(`${protoDir}`);
+
+    const serviceRoot = await loadProtobuf(serviceFiles.filter(f => f.indexOf(".proto") >= 0).map(f => `${protoDir}/services/${f}`));
+    const schemaRoot = await loadProtobuf(schemaFiles.filter(f => f.indexOf(".proto") >= 0).map(f => `${protoDir}/${f}`));
+    const serviceNameList = getAllServices(serviceRoot);
+
+    const endpoints: Endpoint[] = [];
+
+    for (const serviceName of serviceNameList) {
+        const serviceObject = serviceRoot.lookupService(serviceName);
+        console.log(serviceObject);
+        for (const method of serviceObject.methodsArray) {
+            console.log("method", method);
+            endpoints.push({
+                methodName: `${serviceName}.${method.name}`,
+                handlerModuleName: getHandlerNameFromProtoFile(protoDir, serviceObject.filename),
+                handlerMethodName: method.name,
+                requestSchema: schemaRoot.lookup(method.requestType),
+                responseSchema: schemaRoot.lookup(method.responseType)
+            });
+        }
+    }
+    console.log(endpoints);
+    return endpoints;
+}
+
+function getHandlerNameFromProtoFile(protoDir: string, protoFile: string) {
+    return protoFile.substring(protoDir.length, protoFile.length - ".proto".length);
+}
+
+export default async function handleRequest(server: ws.Server) {
+    const endpoints = await initializeEndpoints();
+
     server.on("connection", (client) => {
         const realSocketObject = (<any>client)._socket;
         console.log(`[PWA] New connection from ${realSocketObject.remoteAddress}:${realSocketObject.remotePort}.`);
@@ -21,11 +84,11 @@ export default function handleRequest(server: ws.Server, handlers: HandlerMap, e
             let requestHandled = false;
 
             for (const endpoint of endpoints) {
-                if (request.method == endpoint.moduleName + "." + endpoint.apiName) {
+                if (request.method == endpoint.methodName) {
                     const payload = endpoint.requestSchema.decode(request.payload);
                     console.log(`[PWA] Request #${request.sequence}: ${JSON.stringify(payload)}`);
-                    const handler: any = handlers[endpoint.moduleName];
-                    const responsePayload = endpoint.responseSchema.create(handler[endpoint.apiName](payload));
+                    const handler: any = require(`./dist/api/${endpoint.handlerModuleName}`);
+                    const responsePayload = endpoint.responseSchema.create(handler[endpoint.handlerMethodName](payload));
                     const response = schema.Response.create({sequence: request.sequence, payload: endpoint.responseSchema.encode(responsePayload).finish()});
                     client.send(schema.Response.encode(response).finish());
                     console.log(`[PWA] Replied #${request.sequence}: ${JSON.stringify(responsePayload)}`);
